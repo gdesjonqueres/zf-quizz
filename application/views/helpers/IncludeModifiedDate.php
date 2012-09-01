50fab79562c2b9914e163d4cbc4ed32cc4be714e
gdesjonqueres <g.desjonqueres@gmail.com>
2012-09-01 12:19:52 +1000

    Changed config files for PHP Cloud

---
+++ application/views/helpers/IncludeModifiedDate.php
@@ -0,0 +1,18 @@
+<?php
+
+class Quizz_View_Helper_IncludeModifiedDate extends Zend_View_Helper_Abstract
+{
+
+    public function includeModifiedDate($uri) {
+        $parts = parse_url($uri);
+        return $parts['path'];
+//         $root = getcwd();
+//         $mtime = filemtime($root . $parts['path']);
+//         return preg_replace(
+//             "/(\.[a-z0-9]+)(\?*.*)$/",
+//             '.'.$mtime.'$1$2',
+//             $uri
+//         );
+    }
+
+}
